90717bf2b09b342901662d1b025ee4c1e85104c3
francescosapioli <S203946@studenti.polito.it>
2016-05-09 16:15:57 +0200

    interfaccia grafica finita.

--- Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
+++ Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
@@ -1,5 +1,78 @@
 package it.polito.tdp.lab3.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class SegreteriaStudentiController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmbCorso;
+
+    @FXML
+    private Button btnCompleta;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCerca;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doCerca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCompleta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbCorso != null : "fx:id=\"cmbCorso\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCompleta != null : "fx:id=\"btnCompleta\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+
+        //inizializzare il comboBox, adesso il tipo e' string
+        
+    }
 }
+
