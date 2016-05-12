88ee0528ffb57575fda5ab6f3e339420afa200f8
francescosapioli <S203946@studenti.polito.it>
2016-05-12 20:02:27 +0200

    finita interfaccia grafica

--- FraLab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
+++ FraLab3_SegreteriaStudenti/src/it/polito/tdp/lab3/controller/SegreteriaStudentiController.java
@@ -1,5 +1,94 @@
 package it.polito.tdp.lab3.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.lab3.model.Corso;
+import it.polito.tdp.lab3.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SegreteriaStudentiController {
 	
+	private Model model;
+	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private ComboBox<Corso> cmbCorso;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private Button btnCompleta;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCerca;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doCerca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCompleta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert cmbCorso != null : "fx:id=\"cmbCorso\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCompleta != null : "fx:id=\"btnCompleta\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        
+        
+        this.txtResult.setEditable(false);
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+		
+	}
 }
